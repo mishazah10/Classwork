181af8a118240a246a43f0b04db8a93f5546505a
mishazah10 <157517960+mishazah10@users.noreply.github.com>
2024-03-20 09:55:06 +0000

    changes

---
+++ main10.py
@@ -0,0 +1,13 @@
+
+class DogSpike:
+    name= 'Spike'
+    def __init__(self):
+       self.is_walked = False
+    def walk(self):
+     print('Dog walked')
+     self.is_walked = True
+my_dog = DogSpike()
+print(my_dog.name)
+print(my_dog.is_walked)
+my_dog.walk()
+print(my_dog.is_walked)
